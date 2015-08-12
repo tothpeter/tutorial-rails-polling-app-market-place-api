7fdb27992757def5738172970d9073889b12f1cb
Peter Toth <tothpeter08@gmail.com>
2015-08-12 21:00:48 +0200

    Adds product model bare bones along with some validations

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150808150554) do
+ActiveRecord::Schema.define(version: 20150812132015) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "products", force: :cascade do |t|
+    t.string   "title",      default: ""
+    t.decimal  "price",      default: 0.0
+    t.boolean  "published",  default: false
+    t.integer  "user_id"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+  end
+
+  add_index "products", ["user_id"], name: "index_products_on_user_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -36,4 +47,5 @@ ActiveRecord::Schema.define(version: 20150808150554) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "products", "users"
 end
---
+++ spec/factories/products.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :product do
+    title "Super Product Name"
+    price "9.99"
+    published false
+    user nil
+  end
+end
