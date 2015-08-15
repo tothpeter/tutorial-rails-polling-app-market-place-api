808833621ca57cd4ef04ff0a068791e4965be609
Peter Toth <tothpeter08@gmail.com>
2015-08-16 01:26:40 +0200

    Adds user order has many relation

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,6 +10,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :products, dependent: :destroy
+  has_many :orders, dependent: :destroy
 
   def generate_authentication_token!
     begin
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,6 +18,7 @@ describe User do
   it { should allow_value('example@domain.com').for(:email) }
 
   it { should have_many(:products) }
+  it { should have_many(:orders) }
 
   it { should validate_uniqueness_of(:auth_token)}
 
