9b12c4238feb27e52464678e65b0d8f42bd12571
Peter Toth <tothpeter08@gmail.com>
2015-08-16 02:41:25 +0200

    Adds the create method for the orders controller

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,11 +1,14 @@
 class Order < ActiveRecord::Base
+  before_validation :set_total!
 
   belongs_to :user
 
   has_many :placements
   has_many :products, through: :placements
 
-
-  validates :total, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :user_id, presence: true
+
+  def set_total!
+    self.total = products.map(&:price).sum
+  end
 end
--- spec/factories/orders.rb
+++ spec/factories/orders.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :order do
     user
-    total 9.99
+    total 0
   end
 end
